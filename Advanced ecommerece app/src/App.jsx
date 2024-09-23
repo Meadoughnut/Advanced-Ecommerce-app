230e37eae2b1bd07bb6459d0a289f21bf3d8d6c1
Meadoughnut <127433011+Meadoughnut@users.noreply.github.com>
2024-09-23 12:23:37 -0400

    the styling for each page initiated

--- Advanced ecommerece app/src/App.jsx
+++ Advanced ecommerece app/src/App.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './index.css';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
+      <div className="app-container">
+        {/* Navbar will be displayed on all pages */}
+        <Navbar />
+
+        {/* Main content area for different routes */}
+        <main>
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </main>
+
+        {/* Footer will be displayed on all pages */}
+        <Footer />
+      </div>
     </Router>
   );
 }
 
 export default App;
+
